7e80e4a1cb12eb8b369c9858480d962a9c649b61
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-03 21:43:49 +0300

    feat: using the split method to get divide the string in a list of strings.

--- src/main/java/org/datastructures/Test5.java
+++ src/main/java/org/datastructures/Test5.java
@@ -2,6 +2,8 @@ package org.datastructures;
 
 import java.io.*;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Test5 {
     public static void main(String[] args)  {
@@ -9,38 +11,43 @@ public class Test5 {
         try {
             // Creating a file
             File inputFile = new File("forgestormbreaker.txt");
-            if (inputFile.exists()) {
+            File inputFile1 = new File("forgestormbreaker1.txt");
+            File inputFile2= new File("forgestormbreaker2.txt");
+            File inputFile3= new File("forgestormbreaker3.txt");
+            if (inputFile1.exists()) {
 
                 //file reader to read the content bytes of the file
-                FileReader fileReader = new FileReader(inputFile);
+                FileReader fileReader = new FileReader(inputFile1);
                 //Buffered reader to convert the bytes into strings
                 BufferedReader bufferedReader = new BufferedReader(fileReader);
                 //reading the first line of the file
                 int rows = Integer.parseInt(bufferedReader.readLine());
                 int columns = Integer.parseInt(bufferedReader.readLine());
-                String firstRow;
-                String edges = "";
 
                 // A queue obeys FIFO architecture - First in first out
-                Queue<Character> listEdges = new LinkedList<>();
+                Queue<Integer> listEdges = new LinkedList<>();
 
                 // getting the remaining contents of the file.
+                Pattern pattern = Pattern.compile("\\s");
+                Pattern pattern1 = Pattern.compile("\\s\\s");
                 for (int i = 0; i < rows; i++) {
-                    firstRow = bufferedReader.readLine();
-                    edges += firstRow;
-                }
-                String matrixEdges = edges.replaceAll(" ", "");
+                    String[] firstRow = bufferedReader.readLine().split(" ");
 
-                for (int i = 0; i < matrixEdges.length(); i++) {
-                    listEdges.add(matrixEdges.charAt(i));
+                    for (int j = 0; j < firstRow.length; j++) {
+                        listEdges.add(Integer.parseInt(firstRow[j]));
+
+                    }
+
                 }
+
+
                 //creating a matrix using the files details
                 int[][] matrix = new int[rows][columns];
 
                 for (int i = 0; i < matrix.length; i++) {
                     for (int j = 0; j < matrix[i].length; j++) {
                         // using the remove method to retrieve and remove vertices weight
-                        matrix[i][j] = Character.getNumericValue(listEdges.remove());
+                        matrix[i][j] = listEdges.remove();
                     }
                 }
 
@@ -54,6 +61,9 @@ public class Test5 {
                 System.out.println(matrix.length);
             }else{
                 inputFile.createNewFile();
+                inputFile1.createNewFile();
+                inputFile2.createNewFile();
+                inputFile3.createNewFile();
             }
 
         }
