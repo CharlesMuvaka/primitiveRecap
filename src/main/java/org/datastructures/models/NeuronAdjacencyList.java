58537fef553f3d9e7380939b3b3e23f40cbe2838
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-05 11:22:09 +0300

    feat: define a blue-print to initialise an adjacency list  instance.

---
+++ src/main/java/org/datastructures/models/NeuronAdjacencyList.java
@@ -0,0 +1,9 @@
+package org.datastructures.models;
+
+public class NeuronAdjacencyList {
+    private Neuron[] allNeurons;
+
+    public void shortestPath(Neuron startNeuron, Neuron endNeuron){
+
+    }
+}
